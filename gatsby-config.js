a85a38449ff92bebd0c97f9ff04146c3c4e60d9d
Snikerso <42245963+Snikerso@users.noreply.github.com>
2021-02-25 09:11:46 +0100

    day3

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,16 +5,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-styled-components",
-    {
-      resolve: `gatsby-source-facebook`,
-      options: {
-        places: [`111736127626645`], // Can be either a numeric ID or the URL ID
-        params: {
-          fields: 'hours, posts { message, created_time }', // See Facebooks API to see what you can query for
-        },
-        key: "EAAEuXLZCnp4YBANjnEdMTMgzTOILx2maHum4BZCIJwGBjZATpZB3KoFdVZCZCZCpZBETbnswL8FKv9feqpZAayvljUZAtxM7hYVAlaIzAdRgObLgXHAQtzMHbTbV1OJjrEKmRaluxE0StVGnVoRnYgO1gyJtsNBt8IPExUqNwZC8pkrdd9s5bYMEHX6WnXydXAwIxq7e8zj9lB1sQZDZD", // You will need to create a Facebook application and go through review in order to get an API token.
-        version: '5.0', // The version of the graph API to use. Defaults to 5.0
-      },
+    
     },
     {
       resolve: "gatsby-plugin-google-analytics",
--- src/components/atoms/MessengerChat.js
+++ src/components/atoms/MessengerChat.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+const facebookAppId = "111736127626645"
 
-class MessengerChat extends React.Component {
-    componentDidMount() {
-      window.fbAsyncInit = function() {
-        FB.init({
-            xfbml: true,
-            version: 'v10.0'
-        });
-      };
-  
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    }
-
-    render() {
-      return (
-        <>
-          <div id="fb-root" />
-          <div class="fb-customerchat"
+export function CustomerChat(){
+  useEffect(()=>{
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        xfbml            : true,
+        version          : 'v10.0'
+      });
+    };
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  })
+  return(
+    <>
+      <div id="fb-root" />
+      <div
+        className="fb-customerchat"
         attribution="setup_tool"
-        page_id="111736127626645">
-      </div>
-</>
-      );
-    }
-  } 
-  
-  export default MessengerChat;
+        page_id={facebookAppId}
+      />
+    </>
+  )
+}
+
